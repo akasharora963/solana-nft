b6ff8e64d5f3c8a08fbf0e5eab71a5ccc480e364
Akash Arora <akasharora963@gmail.com>
2024-02-11 19:55:22 +0530

    added: sell nft and test cases flow

--- programs/solana-nft-demo/src/lib.rs
+++ programs/solana-nft-demo/src/lib.rs
@@ -1,19 +1,19 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, system_program};
 use anchor_spl::{
-    associated_token::AssociatedToken,
+    associated_token::{create,Create,AssociatedToken},
     metadata::{
         create_master_edition_v3, create_metadata_accounts_v3, CreateMasterEditionV3,
         CreateMetadataAccountsV3, Metadata,
     },
-    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+    token::{mint_to, transfer, Mint, MintTo, Token, TokenAccount, Transfer},
 };
 
 use mpl_token_metadata::{
     pda::{find_master_edition_account, find_metadata_account},
-    state::DataV2
+    state::DataV2,
 };
 
-declare_id!("4zQgKM8DEnyd9yLkjfPTz2NDJdWKLX53NLCLKcrF7s8Y");
+declare_id!("BB1yPbvMivSpc8NfTnujCdm8wgfaE8bDHJepRFcCYtpk");
 
 #[program]
 pub mod solana_nft_demo {
@@ -37,6 +37,9 @@ pub mod solana_nft_demo {
 
         mint_to(cpi_context, 1)?;
 
+        msg!("Minting token to token account...");
+        msg!("Mint: {}", &ctx.accounts.mint.to_account_info().key());
+
         // create metadata account
         let cpi_context = CpiContext::new(
             ctx.accounts.token_metadata_program.to_account_info(),
@@ -51,26 +54,17 @@ pub mod solana_nft_demo {
             },
         );
 
-        // let data_v2 = DataV2 {
-        //     name: name,
-        //     symbol: symbol,
-        //     uri: uri,
-        //     seller_fee_basis_points: 0,
-        //     creators: None,
-        //     collection: None,
-        //     uses: None,
-        // };
-        let data_v2 = DataV2{
+        let data_v2 = DataV2 {
             name,
             symbol,
             uri,
             seller_fee_basis_points: 0,
             creators: None,
             collection: None,
-            uses: None
+            uses: None,
         };
-        
-        create_metadata_accounts_v3(cpi_context,data_v2 , false, true, None)?;
+
+        create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
 
         //create master edition account
         let cpi_context = CpiContext::new(
@@ -89,6 +83,77 @@ pub mod solana_nft_demo {
         );
 
         create_master_edition_v3(cpi_context, None)?;
+        msg!(
+            "Token Metadata program {:?}",
+            ctx.accounts.token_metadata_program.to_account_info()
+        );
+
+        Ok(())
+    }
+
+    pub fn sell(ctx: Context<SellNft>, sale_lamports: u64) -> Result<()> {
+        msg!("Initiating transfer of {} lamports...", sale_lamports);
+        msg!(
+            "Purchaser (sending lamports): {}",
+            &ctx.accounts.buyer_authority.key()
+        );
+        msg!(
+            "Seller (receiving lamports): {}",
+            &ctx.accounts.owner_authority.key()
+        );
+        system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                system_program::Transfer {
+                    from: ctx.accounts.buyer_authority.to_account_info(),
+                    to: ctx.accounts.owner_authority.to_account_info(),
+                },
+            ),
+            sale_lamports,
+        )?;
+
+        msg!("Lamports transferred successfully.");
+
+        msg!("Creating buyer token account...");
+        msg!(
+            "Buyer Token Address: {}",
+            &ctx.accounts.buyer_token_account.key()
+        );
+        create(CpiContext::new(
+            ctx.accounts.associated_token_program.to_account_info(),
+            Create {
+                payer: ctx.accounts.buyer_authority.to_account_info(),
+                associated_token: ctx.accounts.buyer_token_account.to_account_info(),
+                authority: ctx.accounts.buyer_authority.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+            },
+        ))?;
+
+        msg!("Transferring NFT...");
+        msg!(
+            "Owner Token Address: {}",
+            &ctx.accounts.owner_token_account.key()
+        );
+        msg!(
+            "Buyer Token Address: {}",
+            &ctx.accounts.buyer_token_account.key()
+        );
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.owner_token_account.to_account_info(),
+                    to: ctx.accounts.buyer_token_account.to_account_info(),
+                    authority: ctx.accounts.owner_authority.to_account_info(),
+                },
+            ),
+            1,
+        )?;
+        msg!("NFT transferred successfully.");
+
+        msg!("Sale completed successfully!");
 
         Ok(())
     }
@@ -135,4 +200,23 @@ pub struct MintNFT<'info> {
     pub token_metadata_program: Program<'info, Metadata>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct SellNft<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub owner_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub owner_authority: Signer<'info>,
+    /// CHECK: We're about to create this with Anchor
+    #[account(mut)]
+    pub buyer_token_account: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub buyer_authority: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
--- tests/solana-nft-demo.ts
+++ tests/solana-nft-demo.ts
@@ -1,16 +1,126 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaNftDemo } from "../target/types/solana_nft_demo";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import {
+	findMasterEditionPda,
+	findMetadataPda,
+	mplTokenMetadata,
+	MPL_TOKEN_METADATA_PROGRAM_ID,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { publicKey } from "@metaplex-foundation/umi";
 
-describe("solana-nft-demo", () => {
+import {
+	TOKEN_PROGRAM_ID,
+	ASSOCIATED_TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+
+
+describe("solana-nft-demo", async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
+	const program = anchor.workspace
+		.SolanaNftDemo as Program<SolanaNftDemo>;
 
-  const program = anchor.workspace.SolanaNftDemo as Program<SolanaNftDemo>;
+	const signer = provider.wallet;
+  
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const umi = createUmi("http://127.0.0.1:8899")
+		.use(walletAdapterIdentity(signer))
+		.use(mplTokenMetadata());
+
+	const mint = anchor.web3.Keypair.generate();
+
+  const buyer = anchor.web3.Keypair.generate();
+
+	// Derive the associated token address account for the mint
+	const associatedTokenAccount = await getAssociatedTokenAddress(
+		mint.publicKey,
+		signer.publicKey
+	);
+
+	// derive the metadata account
+	let metadataAccount = findMetadataPda(umi, {
+		mint: publicKey(mint.publicKey),
+	})[0];
+
+	//derive the master edition pda
+	let masterEditionAccount = findMasterEditionPda(umi, {
+		mint: publicKey(mint.publicKey),
+	})[0];
+
+	const metadata = {
+		name: "Gojo",
+		symbol: "JJK",
+		uri: "https://yellow-major-catfish-634.mypinata.cloud/ipfs/QmdjfXJD8MHkE4vXBibGncsT95wB7jvoPJW3DrmpyTUodu",
+	};
+
+	it("mints nft!", async () => {
+		const tx = await program.methods
+			.mintNft(metadata.name, metadata.symbol, metadata.uri)
+			.accounts({
+				signer: provider.publicKey,
+				mint: mint.publicKey,
+				associatedTokenAccount,
+				metadataAccount,
+				masterEditionAccount,
+				tokenProgram: TOKEN_PROGRAM_ID,
+				associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+				tokenMetadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID,
+				systemProgram: anchor.web3.SystemProgram.programId,
+				rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+			})
+			.signers([mint])
+			.rpc({
+        skipPreflight : true
+      });
+
+		console.log(
+			`mint nft tx: https://explorer.solana.com/tx/${tx}?cluster=localnet`
+		);
+		console.log(
+			`minted nft: https://explorer.solana.com/address/${mint.publicKey}?cluster=localnet`
+		);
+	});
+
+  it("Sell!", async () => {
+
+    // Testing constants
+
+    const saleAmount = 1 * anchor.web3.LAMPORTS_PER_SOL;
+    // Derive the associated token account address for owner & buyer
+
+    const ownerTokenAddress = await anchor.utils.token.associatedAddress({
+      mint: mint.publicKey,
+      owner: signer.publicKey
+    });
+    const buyerTokenAddress = await anchor.utils.token.associatedAddress({
+      mint: mint.publicKey,
+      owner: buyer.publicKey,
+    });
+    console.log(`Request to sell NFT: ${mint} for ${saleAmount} lamports.`);
+    console.log(`Owner's Token Address: ${ownerTokenAddress}`);
+    console.log(`Buyer's Token Address: ${buyerTokenAddress}`);
+
+    // Transact with the "sell" function in our on-chain program
+    
+    await program.methods.sell(
+      new anchor.BN(saleAmount)
+    )
+    .accounts({
+      mint: mint.publicKey,
+      ownerTokenAccount: ownerTokenAddress,
+      ownerAuthority: signer.publicKey,
+      buyerTokenAccount: buyerTokenAddress,
+      buyerAuthority: buyer.publicKey,
+    })
+    .signers([buyer])
+    .rpc({
+      skipPreflight: true
+    });
   });
+
 });
